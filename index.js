71776019457a3086ec5adcb1705dcb2edf8da468
o2fx <o2fxstudio@gmail.com>
2020-06-13 12:01:29 +0200

    add / endpoint

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function (req, res) {
+  res.send('Welcome to the test API');
+});
+
 app.get('/users', function (req, res) {
-  res.send({
+  res.json({
     id: 'uuid345"-ab3',
     name: 'toto'
   });
