02ba117664b682a595823d09cedae1d93f16d6d3
o2fx <o2fxstudio@gmail.com>
2020-06-13 11:56:57 +0200

    Change endpoints

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/users', function (req, res) {
+  res.send({
+    id: 'uuid345"-ab3',
+    name: 'toto'
+  });
 });
 
 app.listen(3000, function () {
